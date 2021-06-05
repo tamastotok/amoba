64b58df9a80446bb18c1dea147942923ad5d472c
unknown <yhuniegaming@gmail.com>
2021-06-05 11:41:04 +0200

    redux change to .ts, refactor

---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { createStore, compose } from 'redux';
+import reducers from './reducers';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers());
+
+export default store;
