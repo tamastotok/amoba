6ec2fed16b450ff3c7c22b86d856ef38aa30b8e4
unknown <yhuniegaming@gmail.com>
2021-06-05 11:36:03 +0200

    constants change to .ts, new variables added

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+//  Player  X (blue)
+export const SET_PLAYER_BLUE_NAME = 'SET_PLAYER_BLUE_NAME';
+
+//  Player  O (red)
+export const SET_PLAYER_RED_NAME = 'SET_PLAYER_RED_NAME';
+
+//  Grid Size
+export const SET_GRID_SIZE = 'SET_GRID_SIZE';
+
+//  Square data
+export const SET_SQUARE_DATA = 'SET_SQUARE_DATA';
+
+//  Marks
+export const SELECT_STARTER_MARK = 'SELECT_STARTER_MARK';
+export const SET_NEXT_MARK = 'SET_NEXT_MARK';
+export const RESET_NEXT_MARK = 'RESET_NEXT_MARK';
+
+// Winner
+export const SET_WINNER = 'SET_WINNER';
+
+//  Grid
+export const DISABLE_GRID = 'DISABLE_GRID';
+export const CHANGE_GRID_STATE = 'CHANGE_GRID_STATE';
