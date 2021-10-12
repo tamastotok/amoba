c1c6441252eb2f5ed02f1192252186bf61b01b53
tamastotok <tamastotok.tk@gmail.com>
2021-10-12 22:15:44 +0200

    gridSize fix

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,6 +1,9 @@
 import Button from '@material-ui/core/Button';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setGridSize } from '../store/grid-size/grid-size.action';
 import { buttonStyles } from '../styles/components';
+import { Reducers } from '../types';
 
 interface Props {
   text: string;
@@ -9,10 +12,22 @@ interface Props {
 
 function StartButton({ text, linkTo }: Props) {
   const classes = buttonStyles();
+  const dispatch = useDispatch();
+  const gridSize = useSelector((state: Reducers) => state.gridSize);
+
+  const handleClick = () => {
+    if (gridSize === 0) {
+      dispatch(setGridSize(8));
+    }
+  };
 
   return (
     <Link className={classes.link} to={linkTo}>
-      <Button className={classes.button} variant="outlined">
+      <Button
+        onClick={handleClick}
+        className={classes.button}
+        variant="outlined"
+      >
         {text}
       </Button>
     </Link>
