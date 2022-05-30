12b26a82af2bc021290ccc518c485e1ef90d9647
tamastotok <tamastotok.tk@gmail.com>
2022-05-30 15:54:53 +0200

    switch endpoint to production

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import io from 'socket.io-client';
   example: 'http://localhost:5000'
 */
 
-//const ENDPOINT = 'https://amoba-server.herokuapp.com';
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'https://amoba-server.herokuapp.com';
+//const ENDPOINT = 'http://localhost:5000';
 const socket = io(ENDPOINT);
 
 export default socket;
